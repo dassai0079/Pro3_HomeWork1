dac347ca9da054a0935afb3b4616e985bda053fa
dassai0079 <st3iwa@gmail.com>
2024-12-31 16:50:32 +0900

    ファイルをオープンできるようになった

--- a.c
+++ a.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
 int main(void){
-    printf("\e[2J\e[1;1H");
-    printf("\e[7m");
-    printf("hello world\n");
+    FILE *fp;
+    char fileName[FILENAME_MAX];
+    //printf("\e[2J\e[1;1H"); //ターミナルをクリア=>カーソルを1行1列目に移動
+    //printf("\e[7m");    //文字の背景、色を反転
+    printf("ファイルの名前を入力してください :\t");
+    scanf("%s",fileName);
+    fp=fopen(fileName,"r");
+    if(fp==NULL){
+        //char isMakeFile;
+        printf("そのファイルは存在しません\n");
+        return(0);
+        // printf("新たに作成しますか？ （Y,y/N,n）");
+        // scanf("%c",isMakeFile);
+        // if(isMakeFile=="y" || isMakeFile=="y"){
+        //     fp=fopen(fileName,"w");
+        // }
+    }
+    printf("Open File !\n");
     // printf("\e[37m\e[47m");
     // printf("hello world\n");
     printf("\e[49m\e[39m");
