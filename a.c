0ad3228943cc6f3d3e42972ad5ce9697ba022918
dassai0079 <st3iwa@gmail.com>
2025-01-02 05:07:52 +0900

    a.cにリスト構造の構造体追加 ファイルの文字列表示機能追加 キー入力機能追加

--- a.c
+++ a.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+typedef struct string String;
+struct string{
+    char str[100];
+    String *next;
+};
+
+void setTerminalMode(void) {
+    struct termios new_termios;
+    tcgetattr(STDIN_FILENO, &new_termios);   // 現在の端末設定を取得
+    new_termios.c_lflag &= ~(ICANON|ECHO);   // 非カノニカルモードにする/入力内容を表示しない
+    new_termios.c_cc[VMIN] = 1;              // 最小文字数を1に設定
+    new_termios.c_cc[VTIME] = 1;             // タイムアウトなし
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);  // 設定を即時反映
+}
+
+void resetTerminalMode(void) {
+    struct termios new_termios;
+    tcgetattr(STDIN_FILENO, &new_termios);
+    new_termios.c_lflag |= ICANON;           // カノニカルモードに戻す
+    new_termios.c_lflag |= ECHO;             // 入力内容を表示
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+}
+
+char getKey(void) {
+    char buf;
+    if (read(STDIN_FILENO, &buf, 1) == 1) {
+        return buf;
+    }
+    return 0;  // エラーまたは入力なし
+}
 
 int main(void){
     FILE *fp;
     char fileName[FILENAME_MAX];
-    //printf("\e[2J\e[1;1H"); //ターミナルをクリア=>カーソルを1行1列目に移動
+    char str[10];
+    
+    //ファイルオープン部
+    printf("\e[2J\e[1;1H"); //ターミナルをクリア=>カーソルを1行1列目に移動
     //printf("\e[7m");    //文字の背景、色を反転
     printf("ファイルの名前を入力してください :\t");
     scanf("%s",fileName);
@@ -18,7 +54,28 @@ int main(void){
         //     fp=fopen(fileName,"w");
         // }
     }
-    printf("Open File !\n");
+
+    //ファイル表示部+strに文字を代入していく
+    while(fgets(str, sizeof(str), fp)) {
+        printf("%s", str);
+    }
+
+    //キー入力部
+    printf("Press a key (q to quit)\n");
+    setTerminalMode();  // 非カノニカルモードに設定
+    while (1) {
+        char c = getKey();  // キー入力を取得
+        switch (c)
+        {
+            case 'q':   //qを入力して終了
+                break;
+            default:
+                break;
+        }
+        //printf("You pressed: %c\n", c);
+    }
+    resetTerminalMode();  // 端末設定を元に戻す
+
     fclose(fp);
     // printf("\e[37m\e[47m");
     // printf("hello world\n");
--- keyEnter.c
+++ keyEnter.c
@@ -3,18 +3,16 @@
 #include <termios.h>
 #include <fcntl.h>
 
-void set_terminal_mode(void) {
+void setTerminalMode(void) {
     struct termios new_termios;
     tcgetattr(STDIN_FILENO, &new_termios);  // 現在の端末設定を取得
     new_termios.c_lflag &= ~(ICANON|ECHO);   // 非カノニカルモードにする/入力内容を表示しない
-    //printf("%d\n",new_termios.c_cc[VMIN]);
-    //printf("%d\n",new_termios.c_cc[VTIME]);
     new_termios.c_cc[VMIN] = 1;              // 最小文字数を1に設定
     new_termios.c_cc[VTIME] = 1;             // タイムアウトなし
     tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);  // 設定を即時反映
 }
 
-void reset_terminal_mode(void) {
+void resetTerminalMode(void) {
     struct termios new_termios;
     tcgetattr(STDIN_FILENO, &new_termios);
     new_termios.c_lflag |= ICANON;           // カノニカルモードに戻す
@@ -22,7 +20,7 @@ void reset_terminal_mode(void) {
     tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
 }
 
-char get_key(void) {
+char getKey(void) {
     char buf;
     if (read(STDIN_FILENO, &buf, 1) == 1) {
         return buf;
@@ -34,16 +32,16 @@ int main(void) {
     printf("%d\n",STDIN_FILENO);
     printf("Press a key (q to quit)\n");
 
-    set_terminal_mode();  // 非カノニカルモードに設定
+    setTerminalMode();  // 非カノニカルモードに設定
 
     while (1) {
-        char c = get_key();  // キー入力を取得
+        char c = getKey();  // キー入力を取得
         if (c == 'q') {
             break;  // 'q'が入力されたら終了
         }
         printf("You pressed: %c\n", c);
     }
 
-    reset_terminal_mode();  // 端末設定を元に戻す
+    resetTerminalMode();  // 端末設定を元に戻す
     return 0;
 }
