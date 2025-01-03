9d5ccb5f9f1bb9246dd3d9b10970814f267b7d04
dassai0079 <st3iwa@gmail.com>
2025-01-03 21:11:10 +0900

    表示の調整

--- a.c
+++ a.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
+#include <string.h>
+#define MAX_LEN 30
 
-typedef struct string String;
-struct string{
+typedef struct stringBuffer{
     char str[100];
-    String *next;
-};
+    struct stringBuffer *next;
+}strbuf;
 
 void setTerminalMode(void) {
     struct termios new_termios;
@@ -37,49 +38,73 @@ int main(void){
     FILE *fp;
     char fileName[FILENAME_MAX];
     char str[10];
-    
+    char utility[MAX_LEN];
+    char c;
     //ファイルオープン部
     printf("\e[2J\e[1;1H"); //ターミナルをクリア=>カーソルを1行1列目に移動
-    //printf("\e[7m");    //文字の背景、色を反転
     printf("ファイルの名前を入力してください :\t");
     scanf("%s",fileName);
     fp=fopen(fileName,"r");
     if(fp==NULL){
-        //char isMakeFile;
         printf("そのファイルは存在しません\n");
         return(0);
-        // printf("新たに作成しますか？ （Y,y/N,n）");
-        // scanf("%c",isMakeFile);
-        // if(isMakeFile=="y" || isMakeFile=="y"){
-        //     fp=fopen(fileName,"w");
-        // }
     }
+    printf("\e[2J\e[1;1H"); //ターミナルをクリア=>カーソルを1行1列目に移動
 
+    // //ファイル表示部+strに文字を代入していく
+    // while(fgets(str, sizeof(str), fp)) {
+    //     printf("%s", str);
+    // }
+
+    //キー入力部
+    //printf("Press a key (q to quit)\n");
+
+    printf("\e[7m");    //文字の背景、色を反転
+    printf("%s\n",fileName);
+    printf("\e[0m");
+    fseek(fp, 0, SEEK_SET);  // ファイルポインタを最初に戻す
     //ファイル表示部+strに文字を代入していく
     while(fgets(str, sizeof(str), fp)) {
         printf("%s", str);
     }
 
-    //キー入力部
+    printf("\e[7m");    //文字の背景、色を反転
     printf("Press a key (q to quit)\n");
+
     setTerminalMode();  // 非カノニカルモードに設定
     while (1) {
-        char c = getKey();  // キー入力を取得
+        // printf("\e[7m");    //文字の背景、色を反転
+        // printf("%s\n",fileName);
+        // printf("\e[0m");
+        // fseek(fp, 0, SEEK_SET);  // ファイルポインタを最初に戻す
+        // //ファイル表示部+strに文字を代入していく
+        // while(fgets(str, sizeof(str), fp)) {
+        //     printf("%s", str);
+        // }
+
+        c = getKey();  // キー入力を取得
+        sprintf(utility,"You pressed: %c", c);
+        printf("\e[H\e[J");  // 画面をクリアせず、上書き更新
+        printf("\e[7m");    //文字の背景、色を反転
+        // printf("Press a key (q to quit)\n");
+        printf("%s",utility);
+        printf("\e[0m");
+
         switch (c)
         {
             case 'q':   //qを入力して終了
-                break;
+                resetTerminalMode();  // 端末設定を元に戻す
+                fclose(fp);
+                printf("\n\e[49m\e[39m"); //画面の状態復帰
+                return(0);
             default:
+                printf("%s",utility);
+                //printf("\e[2J\e[1;1H"); //ターミナルをクリア=>カーソルを1行1列目に移動    
                 break;
         }
-        //printf("You pressed: %c\n", c);
     }
     resetTerminalMode();  // 端末設定を元に戻す
-
     fclose(fp);
-    // printf("\e[37m\e[47m");
-    // printf("hello world\n");
     printf("\e[49m\e[39m"); //画面の状態復帰
     return(0);
 }
-
