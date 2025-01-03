24855547eaa23ad22918f80311321a2a4c3c6f09
dassai0079 <st3iwa@gmail.com>
2025-01-03 23:04:09 +0900

    文字列の管理をリスト構造に,キー入力表示がうまく行かない

--- a.c
+++ a.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
-#include <string.h>
+#include <stdlib.h>
 #define MAX_LEN 30
 
 typedef struct stringBuffer{
     char str[100];
+    struct stringBuffer *prev;
     struct stringBuffer *next;
 }strbuf;
 
@@ -14,7 +15,7 @@ void setTerminalMode(void) {
     tcgetattr(STDIN_FILENO, &new_termios);   // 現在の端末設定を取得
     new_termios.c_lflag &= ~(ICANON|ECHO);   // 非カノニカルモードにする/入力内容を表示しない
     new_termios.c_cc[VMIN] = 1;              // 最小文字数を1に設定
-    new_termios.c_cc[VTIME] = 1;             // タイムアウトなし
+    new_termios.c_cc[VTIME] = 0;             // タイムアウトなし
     tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);  // 設定を即時反映
 }
 
@@ -34,12 +35,28 @@ char getKey(void) {
     return 0;  // エラーまたは入力なし
 }
 
+void newNextStr(strbuf* from){ //受け取ったstrbufのnext要素を埋める
+    strbuf* to = malloc(sizeof(strbuf));
+    if(from->next=NULL){
+        to->next=NULL;
+    }else if(from->next!=NULL){
+        to->next=from->next;
+        to->next->prev=to;
+    }
+    from->next=to;
+    to->prev=from;
+}
+
 int main(void){
     FILE *fp;
     char fileName[FILENAME_MAX];
-    char str[10];
     char utility[MAX_LEN];
     char c;
+    strbuf* head = malloc(sizeof(strbuf));
+    head->prev=NULL;
+    head->next=NULL;
+    strbuf* heading=head;
+
     //ファイルオープン部
     printf("\e[2J\e[1;1H"); //ターミナルをクリア=>カーソルを1行1列目に移動
     printf("ファイルの名前を入力してください :\t");
@@ -49,62 +66,39 @@ int main(void){
         printf("そのファイルは存在しません\n");
         return(0);
     }
+
     printf("\e[2J\e[1;1H"); //ターミナルをクリア=>カーソルを1行1列目に移動
-
-    // //ファイル表示部+strに文字を代入していく
-    // while(fgets(str, sizeof(str), fp)) {
-    //     printf("%s", str);
-    // }
-
-    //キー入力部
-    //printf("Press a key (q to quit)\n");
-
     printf("\e[7m");    //文字の背景、色を反転
     printf("%s\n",fileName);
     printf("\e[0m");
-    fseek(fp, 0, SEEK_SET);  // ファイルポインタを最初に戻す
+    //fseek(fp, 0, SEEK_SET);  // ファイルポインタを最初に戻す
     //ファイル表示部+strに文字を代入していく
-    while(fgets(str, sizeof(str), fp)) {
-        printf("%s", str);
+    while(fgets(heading->str, sizeof(heading->str), fp)) {
+        printf("%s", heading->str);
+        newNextStr(heading);
     }
-
     printf("\e[7m");    //文字の背景、色を反転
     printf("Press a key (q to quit)\n");
+    printf("\e[0m");
 
     setTerminalMode();  // 非カノニカルモードに設定
-    while (1) {
-        // printf("\e[7m");    //文字の背景、色を反転
-        // printf("%s\n",fileName);
-        // printf("\e[0m");
-        // fseek(fp, 0, SEEK_SET);  // ファイルポインタを最初に戻す
-        // //ファイル表示部+strに文字を代入していく
-        // while(fgets(str, sizeof(str), fp)) {
-        //     printf("%s", str);
-        // }
 
+    while(1){   //メインループ
         c = getKey();  // キー入力を取得
-        sprintf(utility,"You pressed: %c", c);
-        printf("\e[H\e[J");  // 画面をクリアせず、上書き更新
-        printf("\e[7m");    //文字の背景、色を反転
-        // printf("Press a key (q to quit)\n");
-        printf("%s",utility);
-        printf("\e[0m");
-
-        switch (c)
-        {
-            case 'q':   //qを入力して終了
-                resetTerminalMode();  // 端末設定を元に戻す
-                fclose(fp);
-                printf("\n\e[49m\e[39m"); //画面の状態復帰
-                return(0);
-            default:
-                printf("%s",utility);
-                //printf("\e[2J\e[1;1H"); //ターミナルをクリア=>カーソルを1行1列目に移動    
-                break;
+        printf("%c",c);
+        switch(c){
+        case 'c':
+            int a=10;
+            break;
+        case 'q':
+            resetTerminalMode();  // 端末設定を元に戻す
+            fclose(fp);
+            printf("\e[2J\e[1;1H"); //ターミナルをクリア=>カーソルを1行1列目に移動
+            printf("\n\e[0m"); //画面の状態復帰
+            return(0);
+            break;
+        default:
+            break;
         }
     }
-    resetTerminalMode();  // 端末設定を元に戻す
-    fclose(fp);
-    printf("\e[49m\e[39m"); //画面の状態復帰
-    return(0);
 }
